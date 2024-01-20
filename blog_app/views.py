1959ca2e47c77b183edabce4468f2892805770fd
Luciano Pietrucci <pietruccihlucianoa5tou@gmail.com>
2024-01-20 12:00:18 -0400

    create read

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.views.generic.edit import CreateView
+from  .models import Post
 
-
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name='home.html'
+    model= Post
     
 class AboutPageView(TemplateView):
-        template_name='about.html'
+    template_name='about.html'
+    
+class BlogDetailView(DetailView):
+    model=Post
+    template_name="post_detail.html"
+    
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields= ["title",'author','body']
 
 # Create your views here.
